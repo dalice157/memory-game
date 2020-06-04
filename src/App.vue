6fca1ecc9a6b49c92e6c247f1b6807f868aede02
dalice157 <dalice157@gmail.com>
2020-06-04 17:14:47 +0800

    新增圖片

--- src/App.vue
+++ src/App.vue
@@ -29,14 +29,25 @@
 <script>
 import _ from 'lodash';
 import moment from 'moment';
+import vue from './assets/img/vue.png';
+import express from './assets/img/express.png';
+import mongo from './assets/img/mongodb.png';
+import webpack from './assets/img/webpack.png';
+import babel from './assets/img/babel.png';
+import jade from './assets/img/jade.png';
+import css from './assets/img/css.png';
+import html from './assets/img/html.png';
+import nodejs from './assets/img/nodejs.png';
 let CardTypes = [
-	{ name: "vue", image: "https://vuejs.org/images/logo.png" },
-	{ name: "express", image: "https://coligo.io/images/express.svg" },
-	{ name: "mongo", image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/45/MongoDB-Logo.svg/527px-MongoDB-Logo.svg.png" },
-	{ name: "nodejs", image: "https://worldvectorlogo.com/logos/nodejs-icon.svg" },
-	{ name: "webpack", image: "https://camo.githubusercontent.com/66747a6e05a799aec9c6e04a3e721ca567748e8b/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f313336353838312f313931383337332f32653035373166612d376462632d313165332d383436352d3839356632393164343366652e706e67" },
-	{ name: "babel", image: "https://babeljs.io/images/logo.svg" },
-	//{ name: "jade", image: "http://jade-lang.com/style/jade-logo-header.svg" },
+	{ name: "vue", image: vue },
+	{ name: "express", image: express },
+	{ name: "mongo", image: mongo },
+	{ name: "nodejs", image: nodejs },
+	{ name: "webpack", image: webpack},
+	{ name: "babel", image: babel },
+	{ name: "jade", image: jade },
+  { name: "css", image: css },
+  { name: "html", image: html },
 ];
 let shuffleCards = () => {
 	let cards = [].concat(_.cloneDeep(CardTypes), _.cloneDeep(CardTypes));
